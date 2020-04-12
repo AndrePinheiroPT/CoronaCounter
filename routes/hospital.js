ab112225c1107616c18ab4b323415c3401609eb5
Andre Pinheiro <andreftmarcelo@gmail.com>
2020-04-12 12:46:01 +0100

    Add a public and views past, configure handlebars and static files

--- routes/hospital.js
+++ routes/hospital.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const router = express.Router()
 
+router.get('/', (req, res) => {
+    res.render('login')
+})
+
 router.get('/newcase', (req, res) => {
     res.send('Add new case')
 })
--- server.js
+++ server.js
@@ -1,12 +1,23 @@
 const express = require('express')
 const app = express()
 const hospital = require('./routes/hospital')
+const handlebars = require('express-handlebars')
+const path = require('path')
 
+// Configurations
+    // Handlebars
+    app.engine('handlebars', handlebars({
+        defaultLayout: 'main'
+    }))
+    app.set('view engine', 'handlebars')
+    // Static Files
+    app.use(express.static(path.join(__dirname, 'public')))
+    
 // Routes
 app.use('/hospital', hospital)
 
 app.get('/', (req, res) => {
-    res.redirect('/hospital/newcase')
+    res.redirect('/hospital')
 })
 
 
