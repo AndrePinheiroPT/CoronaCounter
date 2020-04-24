3f635c1fbb50f7d1e8f64e9424e5a625b003324e
Andre Pinheiro <andreftmarcelo@gmail.com>
2020-04-24 23:09:51 +0100

    Upgrade hospital route and home.handlebars

--- routes/hospital.js
+++ routes/hospital.js
@@ -3,7 +3,7 @@ const router = express.Router()
 const logged = require('../helpers/authenticate')
 
 router.get('/', logged, (req, res) => {
-    res.render('hospital/home', {hosp: req.user[0]})
+    res.render('hospital/home', {hospital: req.user[0]})
 })
 
 module.exports = router
