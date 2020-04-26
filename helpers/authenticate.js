90af321837177318e9adfe6d222dfff5b2d8aa55
Andre Pinheiro <andreftmarcelo@gmail.com>
2020-04-26 23:15:48 +0100

    add error messages in login.handlebars and return error messages

--- helpers/authenticate.js
+++ helpers/authenticate.js
@@ -2,6 +2,7 @@ function checkAuthenticate(req, res, next){
     if(req.user){
         next()
     }else{
+        req.flash('error_msg', 'You are not hospital user!')
         res.redirect('/login')
     }
 }
