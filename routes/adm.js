7085448bcc67885d0b7403843c8149d761a464a3
André Pinheiro <andreftmarcelo@gmail.com>
2020-05-01 23:15:46 +0100

    Add contect in home and deleteHosp route

--- routes/adm.js
+++ routes/adm.js
@@ -1,10 +1,13 @@
 const express = require("express")
 const router = express.Router()
-//const checkers = require('../helpers/authenticate')
+const checkers = require('../helpers/authenticate')
 
-router.get('/', (req, res) => {
-    res.send("Welcome to the adm route!")
+router.get('/', checkers.checkAdm, (req, res) => {
+    res.render('adm/home')
 })
 
+router.get('/deleteHosp', (req, res) => {
+    res.send('Delete Hospital')
+})
 
 module.exports = router
