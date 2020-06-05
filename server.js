b418d5d9d0928d571d27d8ff9d28383879803398
André Pinheiro <andreftmarcelo@gmail.com>
2020-06-05 22:55:12 +0100

    Add regitered cases in main route

--- server.js
+++ server.js
@@ -69,6 +69,7 @@ app.get('/', (req, res) => {
             }
         }
         res.render('home',{
+            registered: list.length,
             negative: NPR[0],
             positive: NPR[1],
             recovery: NPR[2]
