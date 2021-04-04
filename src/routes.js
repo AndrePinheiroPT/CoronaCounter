2283a3721e771189655cc41cd1c24c1ed1e28680
Andre Pinheiro <andreftmarcelo@gmail.com>
2021-04-04 16:15:42 +0100

    Add middleware to verify hospitals

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
+const middleware = require('./middleware')
 const { checkHospital, checkAdm } = require('./helpers/authenticate')
 
 const HospitalController = require('./controllers/HospitalController')
@@ -13,6 +14,9 @@ router.get('/hospital', hospitalController.hospitalList)
 router.delete('/remove-hospital', hospitalController.hospitalRemove)
 router.post('/new-hospital', hospitalController.hospitalCreate)
 router.post('/login', hospitalController.hospitalLogin)
+router.get('/test', middleware.verify, (req, res) => {
+    res.status(200).json(req.user)
+})
 
 router.get('/people', peopleController.peopleList)
 router.get('/people-state', peopleController.peopleStates)
