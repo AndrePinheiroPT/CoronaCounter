3b2e264e900a363702ecc7cdc5d00b91fe317005
Andre Pinheiro <andreftmarcelo@gmail.com>
2021-04-05 14:01:37 +0100

    Remove test route and add verify middleware in some routes

--- src/routes.js
+++ src/routes.js
@@ -14,12 +14,9 @@ router.get('/hospital', hospitalController.hospitalList)
 router.delete('/remove-hospital', hospitalController.hospitalRemove)
 router.post('/new-hospital', hospitalController.hospitalCreate)
 router.post('/login', hospitalController.hospitalLogin)
-router.get('/test', middleware.verify, (req, res) => {
-    res.status(200).json(req.user)
-})
 
-router.get('/people', peopleController.peopleList)
 router.get('/people-state', peopleController.peopleStates)
-router.post('/new-person', peopleController.peopleCreate)
+router.get('/people', middleware.verify, peopleController.peopleList)
+router.post('/new-person', middleware.verify, peopleController.peopleCreate)
 
 module.exports = router
